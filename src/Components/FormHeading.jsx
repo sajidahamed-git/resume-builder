9d274cc2b2850e5074db6c1c4e174345f33bd111
Sajid <sajidahamed2001@gmail.com>
2025-03-07 16:07:24 +0530

    first accordion heading done

---
+++ src/Components/FormHeading.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export const FormHeading = () => {
+  return (
+    <div className="items-center mt-6 space-y-4 w-full">
+      {/* Card-like Heading */}
+      <div className="bg-white text-black text-2xl font-bold text-center px-6 py-4 rounded-2xl shadow-lg w-full">
+        Resume Builder
+      </div>
+    </div>
+  );
+};
--- src/Form.jsx
+++ src/Form.jsx
@@ -1,12 +1,27 @@
-import { Button } from "./Components/ui/button";
-
-
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion";
+import { FormHeading } from "./Components/FormHeading";
 function Form() {
   return (
-    <div className="flex justify-center mt-6">
-      <div className="bg-gray-500 text-black  text-2xl font-bold text-center px-6 py-4 rounded-2xl shadow-lg w-3/4">
-        Resume Builder
-      </div>
+    <div className="flex flex-col gap-6">
+      <FormHeading></FormHeading>
+      {/* Accordion Section */}
+      <Accordion
+        type="single"
+        collapsible
+        className="w-full rounded-2xl bg-gray-500 px-4 py-1 text-black"
+      >
+        <AccordionItem value="item-1">
+          <AccordionTrigger>Personal Information</AccordionTrigger>
+          <AccordionContent>
+            Yes, it adheres to accessibility best practices.
+          </AccordionContent>
+        </AccordionItem>
+      </Accordion>
     </div>
   );
 }
