9b06d3aa4f2d9beeb881714f95cb47e1ba62a7d7
Sajid <sajidahamed2001@gmail.com>
2025-03-22 03:15:13 +0530

    ExperienceForm

--- src/Components/Experience/ExperienceForm.jsx
+++ src/Components/Experience/ExperienceForm.jsx
@@ -1,4 +1,4 @@
-import { Button } from "@/components/ui/button";
+import { Button } from "@/Components/ui/button";
 
 function ExperienceForm({ setShowForm, savetoArray }) {
   const saveExperience = () => {
